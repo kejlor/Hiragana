8c01c4c94b57c92ff86dd46977e6f49c2176c2b5
kejlor <woj450@gmail.com>
2021-12-05 21:40:42 +0100

    Initial Commit

---
+++ HiraganaCards/HiraganaCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiraganaCardsApp.swift
+//  HiraganaCards
+//
+//  Created by Bartosz Wojtkowiak on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct HiraganaCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
