bf9854b8d85bb58ea4a17890724d7fd4815ca4d5
kejlor <woj450@gmail.com>
2021-12-05 22:48:09 +0100

    First commit.

--- HiraganaCards/ContentView.swift
+++ HiraganaCards/ContentView.swift
@@ -7,10 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+struct MovementButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(minWidth: 75, minHeight: 20)
             .padding()
+            .foregroundColor(.black)
+            .background(.pink)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func buttonStyle() -> some View {
+        modifier(MovementButton())
+    }
+}
+
+struct ContentView: View {
+    @State private var currentAt = 0
+    @State private var previousDisabled = true
+    @State private var nextDisabled = false
+    @State private var hiraganaChars = ["あ", "い", "う", "え", "お"]
+    @State private var meaningChars = ["A", "I", "U", "E", "O"]
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                Text("Meaning: \(meaningChars[currentAt])")
+                    .font(.title)
+                Text(hiraganaChars[currentAt])
+                    .font(.system(size: 200))
+                    .padding()
+                    .background(Circle()
+                                    .fill(Color.red)
+                                    .frame(width: 250, height: 250))
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button("Previous", action: moveToPrevious)
+                        .buttonStyle()
+                        .disabled(currentAt == 0 ? true : false)
+                    Spacer()
+                    Button("Next", action: moveToNext)
+                        .buttonStyle()
+                        .disabled(currentAt < hiraganaChars.count - 1 ? false : true)
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func moveToNext() {
+        currentAt += 1
+    }
+    
+    func moveToPrevious() {
+        currentAt -= 1
     }
 }
 
